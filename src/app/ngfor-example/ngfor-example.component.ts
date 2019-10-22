4b500bb68ce11439e2e4600fa995b79d571f7571
Carlos Rios <crbarinotto@gmail.com>
2019-10-22 17:48:05 -0500

    ejemplos ngfor

--- src/app/ngfor-example/ngfor-example.component.ts
+++ src/app/ngfor-example/ngfor-example.component.ts
@@ -7,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgforExampleComponent implements OnInit {
 
-  constructor() { }
+  libros:Array<Object>;
+
+  constructor() { 
+    this.libros = [
+      {id:1, titulo:'Da Vinci Code,The', autor:'Brown, Dan'},
+      {id:2, titulo:'Harry Potter and the Deathly Hallows', autor:'Rowling, J.K.'},
+      {id:3, titulo:'Harry Potter and the Philosopher\'s Stone', autor:'Rowling, J.K.'},
+      {id:4, titulo:'Harry Potter and the Order of the Phoenix', autor:'Rowling, J.K.'},
+      {id:5, titulo:'Fifty Shades of Grey', autor:'James, E. L.'},
+      {id:6, titulo:'Harry Potter and the Goblet of Fire', autor:'Rowling, J.K.'},
+      {id:7, titulo:'Harry Potter and the Chamber of Secrets', autor:'Rowling, J.K.'},
+      {id:8, titulo:'Harry Potter and the Prisoner of Azkaban', autor:'Rowling, J.K.'},
+      {id:9, titulo:'Angels and Demons', autor:'Brown, Dan'},
+      {id:10, titulo:'Harry Potter and the Half-blood Prince:Children\'s Edition', autor:'Rowling, J.K.'}
+    ];
+  }
+
+  mostrarAutor(_libro) {
+    alert(` ${_libro.titulo} fue escrito por ${_libro.autor} `);
+  }
 
   ngOnInit() {
   }
